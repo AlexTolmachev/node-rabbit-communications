9f311db0b1cb33b3ce4c0fc2c2941b05ea248221
alextolmachevv <alextolmachevv@gmail.com>
2019-06-22 12:19:54 +0300

    implemented basic Service functionality and covered it with some tests

--- src/Service.js
+++ src/Service.js
@@ -1,5 +1,114 @@
 const RabbitClient = require('rabbit-client');
 
 module.exports = class Service {
+  constructor(options) {
+    if (!options) {
+      throw new Error('No options passed to the Service constructor');
+    }
 
+    const {
+      name,
+      rabbitClient,
+      rabbitOptions,
+      isInputEnabled = true,
+      isOutputEnabled = true,
+      outputMessageTtl = 3e4,
+      namespace = 'rabbit-communications',
+    } = options;
+
+    if (!name) {
+      throw new Error('Service name is required');
+    }
+
+    if (!isInputEnabled && !isOutputEnabled) {
+      throw new Error(`
+        Both input and output cannot be disabled.
+        At least one communication channel is required
+      `);
+    }
+
+    if (!rabbitClient && !rabbitOptions) {
+      throw new Error(`
+        It is necessary to pass to the constructor either your own rabbitClient (RabbitClient instance)
+        or rabbitOptions to create RabbitClient instance within the service.
+      `);
+    }
+
+    this.name = name;
+    this.namespace = namespace;
+    this.rabbitClient = rabbitClient;
+    this.rabbitOptions = rabbitOptions;
+    this.isInputEnabled = isInputEnabled;
+    this.isOutputEnabled = isOutputEnabled;
+    this.outputMessageTtl = outputMessageTtl;
+
+    this.inputQueueName = `${this.name}:input`;
+    this.outputQueueName = `${this.name}:output`;
+  }
+
+  addInputListener(fn) {
+    if (!this.isInputEnabled) {
+      throw new Error('Service input channel is disabled, therefore input listener would never be called');
+    }
+
+    this.inputListener = fn;
+  }
+
+  async send(payload) {
+    if (!this.isOutputEnabled) {
+      throw new Error('Service output channel is disabled, can not send message');
+    }
+
+    return this.outputChannel.publish(this.namespace, this.outputQueueName, payload);
+  }
+
+  async start() {
+    if (!this.rabbitClient) {
+      this.rabbitClient = new RabbitClient(this.rabbitOptions.url, {
+        appName: this.name,
+        json: true,
+      });
+    }
+
+    if (this.isOutputEnabled) {
+      this.outputChannel = await this.rabbitClient.getChannel({
+        onReconnect: async (channel) => {
+          await channel.assertExchange(this.namespace, 'direct');
+          await channel.assertQueue(this.outputQueueName);
+          await channel.bindQueue(this.outputQueueName, this.namespace, this.outputQueueName);
+        },
+      });
+    }
+
+    if (this.isInputEnabled) {
+      if (typeof this.inputListener !== 'function') {
+        throw new Error('Service input is enabled but no listener is provided');
+      }
+
+      this.inputChannel = await this.rabbitClient.getChannel({
+        onReconnect: async (channel) => {
+          await channel.assertExchange(this.namespace, 'direct');
+
+          await channel.assertQueue(this.inputQueueName, {
+            messageTtl: this.outputMessageTtl,
+          });
+
+          await channel.bindQueue(this.inputQueueName, this.namespace, this.inputQueueName);
+
+          // await channel.prefetch(this.rabbitOptions.prefetch); TODO: get prefetch from RabbitClient instance?
+
+          await channel.consume(this.inputQueueName, async (msg, ch, data) => {
+            try {
+              // TODO: pass "this" as second argument to inputListener?
+              await this.inputListener(data, this);
+              await ch.ack(msg);
+            } catch (e) {
+              console.error(e);
+              await ch.nack(msg, false, true); // requeue message
+            }
+          });
+        },
+      });
+    }
+  }
 };
--- test/service.test.js
+++ test/service.test.js
@@ -3,21 +3,86 @@ const { Service, RabbitClient } = require('../src');
 
 const { RABBIT_URL } = process.env;
 
-describe('Service (allows to exchange messages with it in both directions)', () => {
-  before(() => {
 
+// TODO: add namespace constant and after hook exchange deletion
+
+describe('Service (allows to exchange messages with it in both directions)', () => {
+  const NAMESPACE = 'service.test.js';
+
+  const createdQueues = [];
+  let rabbitClient;
+
+  before(() => {
+    rabbitClient = new RabbitClient(RABBIT_URL, {
+      appName: NAMESPACE,
+      json: true,
+    });
   });
 
   after(async () => {
+    const channel = await rabbitClient.getChannel();
 
+    await Promise.all(createdQueues.map(queueName => channel.deleteQueue(queueName)));
+    await channel.deleteExchange(NAMESPACE);
   });
 
   it('start method throws exception if input channel is enabled but no listener provided', async () => {
+    const service = new Service({
+      name: `${NAMESPACE}-0`,
+      isOutputEnabled: false,
+      isInputEnabled: true,
+      namespace: NAMESPACE,
+      rabbitClient,
+    });
 
+    // no service.addInputListener(fn) call here..
+
+    let isErrorCaught = false;
+
+    try {
+      await service.start();
+    } catch (e) {
+      isErrorCaught = true;
+    }
+
+    const { inputQueueName, outputQueueName } = service;
+
+    createdQueues.push(inputQueueName);
+    createdQueues.push(outputQueueName);
+
+    expect(isErrorCaught).to.be.equal(true);
   });
 
   it('creates input and output queues', async () => {
+    const serviceName = `${NAMESPACE}-1`;
 
+    const service = new Service({
+      isOutputEnabled: true,
+      isInputEnabled: true,
+      namespace: NAMESPACE,
+      name: serviceName,
+      rabbitClient,
+    });
+
+    service.addInputListener(() => {}); // required to start service
+
+    await service.start();
+
+    const { inputQueueName, outputQueueName } = service;
+
+    createdQueues.push(inputQueueName);
+    createdQueues.push(outputQueueName);
+
+    expect(inputQueueName).to.be.a('string');
+    expect(outputQueueName).to.be.a('string');
+    expect(inputQueueName).to.be.equal(`${serviceName}:input`);
+    expect(outputQueueName).to.include(`${serviceName}:output`);
+
+    const doesInputQueueExist = await service.inputChannel.checkQueue(inputQueueName);
+    const doesOutputQueueExist = await service.outputChannel.checkQueue(outputQueueName);
+
+    expect(doesInputQueueExist).not.to.be.equal(undefined);
+    expect(doesOutputQueueExist).not.to.be.equal(undefined);
   });
 
   it('allows to turn off input or output functionality', async () => {
