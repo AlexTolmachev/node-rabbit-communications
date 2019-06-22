b9ff63dbde7128345c579300143f36f242bb006d
alextolmachevv <alextolmachevv@gmail.com>
2019-06-22 12:17:58 +0300

    added test cases for manager

---
+++ test/manager.test.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const { Service, CommunicatorManager } = require('../src');
+
+describe('CommunicatorManager (manages a pool of Communicators to interact with multiple services simultaneously. Core gateway functionality)', () => {
+  before(async () => {
+
+  });
+
+  after(async () => {
+
+  });
+
+  it('registers multiple communicators', async () => {
+
+  });
+
+  it('allows to apply async (koa-style) middleware for all incoming messages', async () => {
+
+  });
+
+  it('allows to send messages via specific Communicator (pass single Service name when sending)', async () => {
+
+  });
+
+  it('allows to broadcast same messages via multiple Communicators (pass array of Service names when sending)', async () => {
+
+  });
+});
